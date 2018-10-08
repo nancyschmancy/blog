788649ebc62338c4be9098c0811c40ec31adaef2
Nancy Reyes <nreyes.sf@gmail.com>
2018-10-08 10:24:28 -0700

    Updated index with some crap

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import include, url
 from . import views
+from django.contrib import admin
+from django.urls import path, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-]
+    path('admin/', admin.site.urls),
+    # path('api/', include('mynewapp.urls')),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'build')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +122,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+  os.path.join(BASE_DIR, 'build/static'),
+]
